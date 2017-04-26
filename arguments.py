88c4fea5ec88231c877257efad9f2db43e880e7f
Matt Maeda <matt@casamaeda.com>
2017-04-26 14:24:11 -0700

    refactor cli and argument

--- arguments.py
+++ arguments.py
@@ -1,115 +1,155 @@
+"""
+Class contains the CLI argument implementation details
+"""
 import imp
 import logging.config
 import os
 import sys
 
 MY_PATH = os.path.abspath(os.path.dirname(__file__))
-DEFAULT_INTERFACE_PATH = os.path.join(MY_PATH, "implementation")
+DEFAULT_IMPLEMENTATION_PATH = os.path.join(MY_PATH, "implementation")
 
 logging.config.fileConfig(os.path.join(MY_PATH, 'logging.conf'))
 
 class CLIArgument(object):
-    def __init__(self, interface_name, custom_interface=None):
+    """ Class contains the actual CLI argument implementation details
+    """
+    def __init__(self, cli_prog_name, implementation_options,
+                 cli_argument_namespace, cli_argument_name,
+                 custom_interface=None):
 
-        self.interface = interface_name
-        self.args = {}
-        self.req_args = {}
-        self.functions = {}
-        self.interface_path = DEFAULT_INTERFACE_PATH
+        self.cli_prog_name = cli_prog_name
+        self.implementation_options = implementation_options
+        self.cli_argument_namespace = cli_argument_namespace
+        self.cli_argument_name = cli_argument_name
+        self.cli_arguments = {}
+        self.required_cli_arguments = {}
+        self.cli_functions = {}
+        self.implementation_path = DEFAULT_IMPLEMENTATION_PATH
 
         if custom_interface is not None:
             logging.info("Loading custom interface " \
                          "directory {}".format(custom_interface))
-            self.interface_path = custom_interface
+            self.implementation_path = custom_interface
+
+        self.load_implementation_options()
 
 
-    def init(self, interfaces, namespace, command):
-
-        for i in interfaces:
-            self.req_args[i] = []
-            exists = None
-
-            i_path = os.path.join(self.interface_path, i)
-            n_path = os.path.join(i_path, namespace)
-            c_path = os.path.join(n_path, command)
-
-            if not os.path.exists(i_path):
-                exists = False
-                msg = "Missing interface {}".format(i)
-                logging.warning(msg)
-
-            elif not os.path.exists(n_path):
-                exists = False
-                msg = "Missing namespace {} for " \
-                    "interface {}".format(namespace, i)
-                logging.warning(msg)
-
-            elif not os.path.exists(c_path):
-                exists = False
-                msg = "Missing command {} on namespace " \
-                    "{} for interface {}".format(command, namespace, i)
-                logging.warning(msg)
-
-            else:
-                exists = True
-                mod = imp.load_source('implementation', c_path)
-
-                for a in getattr(mod, "ARGS"):
-                    n = a.get("name")
-                    r = a.get("required", False)
-                    h = a.get("help", "Help not defined for {}".format(n))
-
-                    self.args[n] = {
-                        "name": "--{}".format(n),
-                        "help": h
-                    }
-
-                    if r:
-                        self.req_args[i].append({"name": n, "help": h})
-
-                self.functions[i] = getattr(mod, "execute")
-
-            if not exists:
-                self.functions[i] = self.command_not_implemented
+    def load_implementation_options(self):
+        """ Iterates through the various implementation options
+            and loads the implementation to the CLI parser
+        """
+        for namespace_option in self.implementation_options:
+            self.setup_implementation(namespace_option)
 
 
-    def add_parser_args(self, parser):
-        for k, v in self.args.iteritems():
-            parser.add_argument(v.get("name"), dest=k, help=v.get("help"))
+    def setup_implementation(self, namespace_option):
+        """ Checks the implementation setting and then loads it
+            if it is implemented
+        """
+        namespace_command_path = os.path.join(self.implementation_path,
+                                              namespace_option,
+                                              self.cli_argument_namespace,
+                                              self.cli_argument_name)
+
+        if not os.path.exists(namespace_command_path):
+            msg = "Missing command {} on namespace {} for " \
+                  "implementation {} at {}".format(self.cli_argument_name,
+                                                   self.cli_argument_namespace,
+                                                   namespace_option,
+                                                   namespace_command_path)
+            logging.warning(msg)
+            self.cli_functions[namespace_option] = self.command_not_implemented
+
+        else:
+            self.load_command_implementation(namespace_option,
+                                             namespace_command_path)
+
+
+    def command_not_implemented(self, user_entered_cli_args):
+        """ This function is called when the command is not implemented """
+        raise NotImplementedError("Command is not implemented")
+
+
+    def load_command_implementation(self, namespace_option,
+                                    namespace_command_path):
+        """ Loads the implementation of the command for the given
+            namespace and its corresponding function call
+        """
+        module = imp.load_source('implementation', namespace_command_path)
+        self.load_implementation_arguments(namespace_option,
+                                           getattr(module, "ARGS"))
+        self.cli_functions[namespace_option] = getattr(module, "execute")
+
+
+    def load_implementation_arguments(self, namespace_option,
+                                      module_arguments):
+        """ Loads the implementation arguments for the command """
+        for implementation_argument in module_arguments:
+            arg_name = implementation_argument.get("name")
+            required = implementation_argument.get("required", False)
+            help_text = implementation_argument.get("help",
+                                                    "Help not defined")
+
+            self.cli_arguments[arg_name] = {
+                "name": "--{}".format(arg_name),
+                "help": help_text
+            }
+
+            if required:
+                req_arg = {"name": arg_name, "help": help_text}
+
+                if namespace_option not in self.required_cli_arguments:
+                    self.required_cli_arguments[namespace_option] = []
+
+                self.required_cli_arguments[namespace_option].append(req_arg)
+
+
+    def attach_arguments_to_parser(self, parser):
+        """ Accepts a parser and attaches the implemented arguments
+            and execute function
+        """
+        for destination, argument in self.cli_arguments.iteritems():
+            parser.add_argument(argument.get("name"),
+                                dest=destination,
+                                help=argument.get("help"))
 
         parser.set_defaults(func=self.execute)
 
 
-    def execute(self, parser, args):
-        interface = getattr(args, self.interface)
-        self.validate(parser, interface, args)
-        return self.functions[interface](args)
+    def execute(self, parser, user_entered_args):
+        """ Validates the user entered args and if valid
+            executes the attached implemented function
+        """
+        impl = getattr(user_entered_args, self.cli_prog_name)
+        self.validate(parser, impl, user_entered_args)
+        return self.cli_functions[impl](user_entered_args)
 
 
-    def validate(self, parser, interface, args):
+    def validate(self, parser, implementation, args):
+        """ Validates that any required argument is present
+        """
+        valid = True
         missing_values = []
 
-        for a in self.req_args[interface]:
-            if getattr(args, a.get("name")) is None:
+        for arg in self.required_cli_arguments[implementation]:
+            if getattr(args, arg.get("name")) is None:
+                valid = False
                 missing_values.append({
-                    "name": a.get("name"),
-                    "help": a.get("help")})
+                    "name": arg.get("name"),
+                    "help": arg.get("help")})
 
-
-        if len(missing_values) > 0:
+        if not valid:
             parser.print_help()
             self.print_additional_usage(missing_values)
             sys.exit(1)
 
 
-    def command_not_implemented(self, args):
-        raise NotImplementedError("Command is not implemented")
-
-
     def print_additional_usage(self, required_args):
-        fmt_usage = map(lambda a: "  --{}\t\t{}".format(a.get("name"),
-                                                        a.get("help")),
-                        required_args)
+        """ Prints the additional required implementation arguments
+        """
+        fmt_usage = ["--{}\t\t{}".format(a.get("name"), a.get("help"))
+                     for a in required_args]
 
         print ""
         print "interface required arguments:"
--- cli.py
+++ cli.py
@@ -1,51 +1,93 @@
-from arguments import CLIArgument
-
+"""
+Class builds the CLI object
+"""
 import argparse
 import logging.config
 import os
 import yaml
+from arguments import CLIArgument
 
 MY_PATH = os.path.abspath(os.path.dirname(__file__))
 
 logging.config.fileConfig(os.path.join(MY_PATH, 'logging.conf'))
 
+
 class CLI(object):
     """ Contains the primary CLI builder function that serves as the entry point
         for the CLI framework
     """
+
     def __init__(self, cli_config_file):
-        self.config = cli_config_file
-        self.cli = None
+        config = self.load_cli_config_file(cli_config_file)
+
+        self.cli = argparse.ArgumentParser(
+            prog=config.get("entry").get("name"))
+
+        cli_implementations = config.get("implementations")
+        self.cli_implementation_name = cli_implementations.get("name")
+        self.cli_implementation_options = cli_implementations.get("options")
+
+        self.set_implementation_cli_name(cli_implementations.get("help"))
+        self.set_cli_subparser_arguments(config.get("arguments"))
 
 
-    def init(self):
-        """ Returns the argument parser object """
-        cfg = None
+    def load_cli_config_file(self, cli_config_file):
+        """ Loads the YAML config file for CLI """
+        config = None
 
-        logging.info("Opening configuration file at {}".format(self.config))
-        with open(self.config, "r") as fh:
-            cfg = yaml.load(fh)
+        logging.info(
+            "Opening configuration file at {}".format(cli_config_file))
+        with open(cli_config_file, "r") as handle:
+            config = yaml.load(handle)
 
-        self.cli = argparse.ArgumentParser(prog=cfg.get("entry").get("name"))
+        return config
 
-        i = cfg.get("interfaces")
-        self.cli.add_argument(i.get("name"), help=i.get("help"),
-                              choices=map(lambda a: a, i.get("options")))
 
-        sp = self.cli.add_subparsers(help="Additional arguments")
+    def set_implementation_cli_name(self, implementation_help_text):
+        """ Sets the CLI PROG name """
+        self.cli.add_argument(self.cli_implementation_name,
+                              help=implementation_help_text,
+                              choices=self.cli_implementation_options)
 
-        for a in cfg.get("arguments"):
-            p = sp.add_parser(a.get("name"), help=a.get("help"))
-            sub_p = p.add_subparsers(help="{} options".format(a.get("name")))
 
-            for o in a.get("options"):
-                opt = sub_p.add_parser(o)
+    def set_cli_subparser_arguments(self, cli_argument_settings):
+        """ Sets up the subparser for the CLI """
+        subparser = self.cli.add_subparsers(help="Additional arguments")
 
-                c = CLIArgument(i.get("name"))
-                c.init(i.get("options"), a.get("name"), o)
-                c.add_parser_args(opt)
+        for cli_argument_setting in cli_argument_settings:
+            cli_namespace_option = cli_argument_setting.get("name")
+            parser = subparser.add_parser(cli_namespace_option,
+                                          help=cli_argument_setting.get("help"))
+
+            self.set_cli_parser(parser, cli_namespace_option,
+                                cli_argument_setting.get("options"))
+
+
+    def set_cli_parser(self, cli_parser, cli_namespace_option, cli_parser_options):
+        """ Sets up the individual subparser parsers for CLI """
+        help_text = "{} options".format(cli_namespace_option)
+        cli_subparser = cli_parser.add_subparsers(help=help_text)
+
+        for cli_argument_option_name in cli_parser_options:
+            optional_argument_parser = cli_subparser.add_parser(cli_argument_option_name)
+
+            # This sets the actual parser option implementation to the CLI
+            self.set_cli_parser_option(optional_argument_parser,
+                                       cli_namespace_option, cli_argument_option_name)
+
+
+    def set_cli_parser_option(self, optional_argument_parser,
+                              cli_namespace_option, cli_argument_option_name):
+        """ Sets the parser option implmentation """
+        cli_arg_implementation = CLIArgument(self.cli_implementation_name,
+                                             self.cli_implementation_options,
+                                             cli_namespace_option,
+                                             cli_argument_option_name)
+
+        cli_arg_implementation.attach_arguments_to_parser(optional_argument_parser)
 
 
     def run(self):
+        """ Parses arguments and passes arguments to mapped function """
         args = self.cli.parse_args()
         args.func(self.cli, args)
